e3a236e5e91df1361708878a9021a326ac2f4d75
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-27 07:31:53 +0800

    014 Provide + Inject for Functions  Methods

--- src/components/KnowledgeGrid.vue
+++ src/components/KnowledgeGrid.vue
@@ -6,7 +6,7 @@
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
-      @select-topic="$emit('select-topic', $event)"
+     
     ></knowledge-element>
   </ul>
 </template>
@@ -14,6 +14,5 @@
 <script>
 export default {
   inject: ['topics'],
-  emits: ['select-topic']
 };
 </script>
